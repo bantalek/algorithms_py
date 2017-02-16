de7b0846ad7dce863c099615c8102b0346fdbf30
bantalek <benjaminolki@gmail.com>
2017-02-16 08:43:51 -0800

    fix case

--- to_camel_case.py
+++ to_camel_case.py
@@ -1,6 +1,6 @@
-def toCamel(str): 
-    iterable = str.split(" ")
-
-    return iterable[0] + "".join([word[0].upper() + word[1:] for word in iterable[1:]])
+def to_camel(str): 
+    words = str.split(" ")
+    first_word = words[0]
+    return  first_word + "".join([word[0].upper() + word[1:] for word in words[1:]])
 
 print(toCamel("i want to be camel case"))
